83c778df5c716b7093d189404525d315d8193757
ryansreel <ryansreel@gmail.com>
2025-11-07 11:36:16 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ScoreSpace36
+
+Unreal Engine 5.6.1
---
+++ Source/ScoreSpace36/ScoreSpace36.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ScoreSpace36.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ScoreSpace36, "ScoreSpace36" );
